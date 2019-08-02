fe0a2e7d99fd42e0424608c68aa98dae5a7f5819
childressdev <jcampbell@childressagency.com>
2019-08-02 17:34:46 -0400

    added print coupon

--- dev/js/custom-scripts.js
+++ dev/js/custom-scripts.js
@@ -1,5 +1,14 @@
 jQuery(document).ready(function($){
   $('.carousel-heights .carousel-inner .carousel-item').carouselHeights();
+
+  $('.coupon').on('click', function(e){
+    e.preventDefault();
+
+    var $coupon = document.getElementById($(this).attr('id'));
+
+    printCoupon($coupon);
+    window.print();
+  });
 });
 
 $.fn.carouselHeights = function () {
@@ -30,3 +39,30 @@ $.fn.carouselHeights = function () {
     normalizeHeights(); //run it again 
   });
 };
+
+function printCoupon(elem, append, delimiter) {
+  var domClone = elem.cloneNode(true);
+
+  var $printSection = document.getElementById("printSection");
+
+  if (!$printSection) {
+    var $printSection = document.createElement("div");
+    $printSection.id = "printSection";
+    document.body.appendChild($printSection);
+  }
+
+  if (append !== true) {
+    $printSection.innerHTML = "";
+  }
+
+  else if (append === true) {
+    if (typeof (delimiter) === "string") {
+      $printSection.innerHTML += delimiter;
+    }
+    else if (typeof (delimiter) === "object") {
+      $printSection.appendChild(delimiter);
+    }
+  }
+
+  $printSection.appendChild(domClone);
+}
