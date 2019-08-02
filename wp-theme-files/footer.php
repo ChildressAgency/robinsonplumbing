0bc4fd17178ccb2caf48356016b83e4cf55879d4
childressdev <jcampbell@childressagency.com>
2019-08-02 17:56:24 -0400

    finished

--- wp-theme-files/footer.php
+++ wp-theme-files/footer.php
@@ -36,38 +36,59 @@
       <div class="container">
         <div class="row">
           <div class="col-md-4">
-            <h4>BROWSE OUR WEBSITE</h4>
-            <ul id="footer-nav">
-              <li><a href="#">Home</a></li>
-              <li><a href="#">About Us</a></li>
-              <li><a href="#">Specials & Discounts</a></li>
-              <li><a href="#">Our Services</a></li>
-              <li><a href="#">Contact Us</a></li>
-            </ul>
+            <h4><?php echo esc_html__('BROWSE OUR WEBSITE', 'robinsonplumbing'); ?></h4>
+            <?php
+              $footer_nav_args = array(
+                'theme_location' => 'footer-nav',
+                'menu' => '',
+                'container' => '',
+                'menu_id' => 'footer-nav',
+                'menu_class' => '',
+                'echo' => true,
+                'fallback_cb' => 'robinsonplumbing_footer_fallback_menu',
+                'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                'depth' => 1
+              );
+              wp_nav_menu($footer_nav_args);
+            ?>
           </div>
           <div class="col-md-4">
-            <h4>CONTACT INFORMATION</h4>
-            <p>Robinson Plumbing<br />Phone: 540-972-9952</p>
-            <h4>HOURS OF OPERATION</h4>
-            <p>Mon - Fri: 9:00 am - 5:00pm<br />Sat - Sun: Appointment Only</p>
+            <h4><?php echo esc_html__('CONTACT INFORMATION', 'robinsonplumbing'); ?></h4>
+            <p><?php echo esc_html(bloginfo('name')); ?><br /><?php echo esc_html__('Phone:', 'robinsonplumbing'); ?> <?php echo esc_html(get_field('main_phone_number', 'option')); ?></p>
+            <h4><?php echo esc_html__('HOURS OF OPERATION', 'robinsonplumbing'); ?></h4>
+            <?php echo wp_kses_post(get_field('hours_of_operation', 'option')); ?>
           </div>
           <div class="col-md-4">
-            <h4>STAY CONNECTED</h4>
+            <h4><?php echo esc_html__('STAY CONNECTED', 'robinsonplumbing'); ?></h4>
             <div class="social">
-              <a href="#" target="_blank"><i class="fab fa-facebook"></i><span class="sr-only">Facebook</span></a>
-              <a href="#" target="_blank"><i class="fab fa-twitter"></i><span class="sr-only">Twitter</span></a>
-              <a href="#" target="_blank"><i class="fab fa-instagram"></i><span class="sr-only">Instagram</span></a>
+              <?php
+                $facebook = get_field('facebook', 'option');
+                $twitter = get_field('twitter', 'option');
+                $instagram = get_field('instagram', 'option');
+
+                if($facebook): ?>
+                  <a href="<?php echo esc_url($facebook); ?>" target="_blank"><i class="fab fa-facebook"></i><span class="sr-only">Facebook</span></a>
+              <?php endif; if($twitter): ?>
+                <a href="<?php echo esc_url($twitter); ?>" target="_blank"><i class="fab fa-twitter"></i><span class="sr-only">Twitter</span></a>
+              <?php endif; if($instagram): ?>
+                <a href="<?php echo esc_url($instagram); ?>" target="_blank"><i class="fab fa-instagram"></i><span class="sr-only">Instagram</span></a>
+              <?php endif; ?>
             </div>
-            <h4 class="mt-4">For Emergencies Call</h4>
-            <p class="emergency-phone">540-972-9952</p>
+            <h4 class="mt-4"><?php echo esc_html__('For Emergencies Call', 'robinsonplumbing'); ?></h4>
+            <p class="emergency-phone"><?php echo esc_html(get_field('emergency_phone', 'option')); ?></p>
           </div>
         </div>
       </div>
     </div>
     <div id="colophon">
-      <p>&copy; Robinson Plumbing</p>
-      <p>Content, including images, displayed on this website is protected by copyright laws. Downloading, republication, retransmission or reproduction of content on this website is strictly prohibited.</p>
-      <p><a href="#">Terms of Use</a>&nbsp;|&nbsp;<a href="#">Privacy Policy</a></p>
+      <p>&copy; <?php echo esc_html(bloginfo('name')); ?></p>
+      <?php 
+        $copyright_statement = get_field('copyright_statement', 'option'); 
+        if($copyright_statement){
+          echo wp_kses_post($copyright_statement);
+        }
+      ?>
+      <p><a href="<?php echo esc_url(home_url('terms-of-use')); ?>"><?php echo esc_html__('Terms of Use', 'robinsonplumbing'); ?></a>&nbsp;|&nbsp;<a href="<?php echo esc_url(home_url('privacy-policy')); ?>"><?php echo esc_html__('Privacy Policy', 'robinsonplumbing'); ?></a></p>
       <p>Website created by <a href="https://childressagency.com" target="_blank">The Childress Agency</a></p>
     </div>
   </footer>
