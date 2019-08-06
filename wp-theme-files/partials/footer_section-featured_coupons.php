df7fcf5c03e137018bf0092e6fcb9b209df7ddf9
childressdev <jcampbell@childressagency.com>
2019-08-06 14:04:44 -0400

    moved coupon loop item to sep temp

--- wp-theme-files/partials/footer_section-featured_coupons.php
+++ wp-theme-files/partials/footer_section-featured_coupons.php
@@ -18,27 +18,13 @@
 
         <div class="coupons d-flex flex-wrap justify-content-around align-items-stretch">
           <?php while(have_rows('coupons', $coupons_page_id)): the_row(); ?>
+
             <?php
-              $coupon_img_url = get_stylesheet_directory_uri() . '/images/plumber-fixing-water-heater.jpg';
-              if(get_sub_field('coupon_image')){
-                $coupon_img = get_sub_field('coupon_image');
-                $coupon_img_url = $coupon_img['url'];
+              if(get_sub_field('featured_coupon')){
+                get_template_part('partials/loop_item', 'coupon'); 
               }
             ?>
-            <a href="#" class="coupon" style="background-image:url(<?php echo esc_url($coupon_img_url); ?>);">
-              <div class="coupon-body d-flex flex-column justify-content-center">
-                <h4><?php echo esc_html(get_sub_field('coupon_title')); ?></h4>
-                <span class="coupon-price"><?php echo esc_html(get_sub_field('coupon_value')); ?></span>
-                <p><?php echo wp_kses_post(get_sub_field('coupon_description')); ?></p>
-              </div>
-                <div class="light-overlay d-print-none"></div>
-              <div class="coupon-footer">
-                <p><?php echo esc_html__('Get Coupon', 'robinsonplumbing'); ?></p>
-              </div>
-              <div class="coupon-print-footer">
-                <p>Robinson Plumbing - <?php echo esc_html(get_sub_field('coupon_location')); ?><br /><?php echo esc_html(get_sub_field('coupon_location_phone_number')); ?></p>
-              </div>
-            </a>
+
           <?php endwhile; ?>
         </div>
       </div>
